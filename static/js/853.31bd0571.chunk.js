ac3a5bcc2c52ef66f1c2c7917b444d362de6bf73
kimoony <hoonby9011@gmail.com>
2022-10-27 02:08:53 +0900

    Updates

---
+++ static/js/853.31bd0571.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_home_manager=self.webpackChunkmy_home_manager||[]).push([[853],{3853:function(t,a,e){e.d(a,{vPQ:function(){return r}});var n=e(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"}},{tag:"path",attr:{d:"M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"}}]})(t)}}}]);
+//# sourceMappingURL=853.31bd0571.chunk.js.map
